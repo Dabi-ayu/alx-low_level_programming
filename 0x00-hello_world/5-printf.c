b45720c97c032b38f36335e562175d33cdb71531
Dabi-ayu <dabiayu8@gmail.com>
2023-04-21 06:56:56 -0700

    printing using the printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - access point 
+ * Description: this function prints using the print function
+ * Return: this returns zero always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
