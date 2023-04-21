81991b02ddabe765047ced029ce7d9de6675e574
Dabi-ayu <dabiayu8@gmail.com>
2023-04-21 15:30:17 -0700

    prints size of special chars

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,14 @@
 /**
  * main - the access point
  * Description: this prints the sizes integers
- * Return: always success 
+ * Return: always succes
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int il;
-	long long int lli;
-	float p;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(il));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(p));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
