af9763a347a2389ab3a53378ae4cae66952ecbd0
Dabi-ayu <dabiayu8@gmail.com>
2023-04-22 03:11:19 -0700

    writting into the error output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - acess point
+ * Description: this will print on the error output
+ * Return: this return one
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
