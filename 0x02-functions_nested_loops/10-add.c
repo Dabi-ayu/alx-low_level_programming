5d84706cd5a6adf5586b720e29ace902cfbbbda3
Dabi-ayu <dabiayu8@gmail.com>
2023-04-28 04:25:26 -0700

    adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - access to addfunc
+ * @a: first number
+ * @b: second number
+ * Description: adds two numbers
+ * Return: the answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
