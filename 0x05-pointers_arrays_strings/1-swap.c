150e700679549ca7db0ba99b702d64423978d280
Dabi-ayu <dabiayu8@gmail.com>
2023-05-02 19:42:01 -0700

    swap pointer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function to swap values
+ * @a: first parameter
+ * @b: second parameter
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = *a;
+}
