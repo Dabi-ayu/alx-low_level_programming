382ff2a00a9bcbe846052f118b59349377fc37bb
Dabi-ayu <dabiayu8@gmail.com>
2023-05-02 19:27:47 -0700

    changes pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - acces point to function
+ * @n: pointer decleared
+ * Description: changes pointer value to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
