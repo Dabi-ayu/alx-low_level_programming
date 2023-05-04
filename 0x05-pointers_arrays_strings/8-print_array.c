0f9edd7060e251a27114625428c5b36c23b56a7f
Dabi-ayu <dabiayu8@gmail.com>
2023-05-03 20:01:07 -0700

    prints array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_array - access
+ * @a: array
+ * @n: parameter
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		if (index != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
+
